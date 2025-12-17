efa41275e6c204575a84d4d1a35cff456ba304b9
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2025-12-17 17:00:32 +0100

    create BoardRender class and make table in placeholder

---
+++ src/boardRender.js
@@ -0,0 +1,26 @@
+import DomUtils from "./domUtils";
+
+
+export class BoardRender {
+
+    static load(gameboard, placeholder) {
+        const table = BoardRender.#createTable(gameboard, placeholder);
+
+        return table;
+    }
+
+    static #createTable(gameboard, placeholder) {
+        const table = DomUtils.createElement('table');
+
+        for (let i = 0; i < gameboard.board.length; i++) {
+            const row = DomUtils.createElement("tr");
+            for (let j = 0; j < gameboard.board[0].length; j++) {
+                const cell = DomUtils.createElement("td", {dataset: {x: i, y: j}});
+                DomUtils.appendToParent(row, cell);
+            }
+            DomUtils.appendToParent(table, row);
+        }
+        DomUtils.appendToParent(placeholder, table);
+    }
+
+}
