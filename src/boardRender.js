c4671cfb75fcedda0d091c5230ff89871279f347
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2025-12-19 01:13:27 +0100

    remove addEventListeners function

--- src/boardRender.js
+++ src/boardRender.js
@@ -1,46 +1,27 @@
 import DomUtils from "./domUtils";
-import { Ship } from "./ship";
 
 export class BoardRender {
+	static load(gameboard, placeholder) {
+		const table = BoardRender.#createTable(gameboard, placeholder);
 
-    static load(gameboard, placeholder) {
-        const table = BoardRender.#createTable(gameboard, placeholder);
+		return table;
+	}
 
-        BoardRender.#addEventListeners(gameboard, table);
+	static #createTable(gameboard, placeholder) {
+		const table = DomUtils.createElement("table");
 
-        return table;
-    }
+		for (let i = 0; i < gameboard.board.length; i++) {
+			const row = DomUtils.createElement("tr");
+			for (let j = 0; j < gameboard.board[0].length; j++) {
+				const cell = DomUtils.createElement("td", {
+					dataset: { x: i, y: j },
+				});
+				DomUtils.appendToParent(row, cell);
+			}
+			DomUtils.appendToParent(table, row);
+		}
+		DomUtils.appendToParent(placeholder, table);
 
-    static #createTable(gameboard, placeholder) {
-        const table = DomUtils.createElement('table');
-
-        for (let i = 0; i < gameboard.board.length; i++) {
-            const row = DomUtils.createElement("tr");
-            for (let j = 0; j < gameboard.board[0].length; j++) {
-                const cell = DomUtils.createElement("td", {dataset: {x: i, y: j}});
-                DomUtils.appendToParent(row, cell);
-            }
-            DomUtils.appendToParent(table, row);
-        }
-        DomUtils.appendToParent(placeholder, table);
-
-        return table;
-    }
-
-    static #addEventListeners(gameboard, table) {
-        for (let i = 0; i < table.rows.length; i++) {
-            for (let j = 0; j < table.rows[i].cells.length; j++) {
-                const cell = table.rows[i].cells[j];
-                cell.addEventListener("click", (event) => {
-                    let x = Number(cell.dataset.x);
-                    let y = Number(cell.dataset.y);
-                    gameboard.receiveAttack(x, y);
-                    if (gameboard.board[x][y] instanceof Ship) event.target.className = "hit";
-                    else event.target.className = "miss";
-                })
-            }
-        }
-    }
-
-
+		return table;
+	}
 }
--- src/boardReneder.test.js
+++ src/boardReneder.test.js
@@ -4,40 +4,18 @@ import DomUtils from "./domUtils";
 import "@testing-library/jest-dom";
 
 describe("BoardRender class", () => {
-    test("check existance", () => {
-        expect(BoardRender).toBeDefined();
-    })
+	test("check existance", () => {
+		expect(BoardRender).toBeDefined();
+	});
 
-    describe("load func", () => {
-        
+	describe("load func", () => {
+		test("creates table", () => {
+			const gameboard = new Gameboard();
+			const placeholder = DomUtils.createElement("div");
 
-        test("creates table", () => {
-            const gameboard = new Gameboard();
-            const placeholder = DomUtils.createElement("div");
+			const table = BoardRender.load(gameboard, placeholder);
 
-            const table = BoardRender.load(gameboard, placeholder);
-
-            expect(table).not.toBeUndefined();
-        })
-
-        test("add event listener that calls recieve attack for that position on gameboard", () => {
-            const gameboard = new Gameboard();
-            const placeholder = DomUtils.createElement("div");
-            
-            const table = BoardRender.load(gameboard, placeholder);
-
-            table.rows[3].cells[5].click();
-
-            expect(gameboard.board[3][5]).toBe("miss");
-            expect(table.rows[3].cells[5].className).toBe("miss");
-
-            gameboard.placeShip([7, 7], [7, 7]);
-
-            table.rows[7].cells[7].click();
-
-            expect(gameboard.board[7][7].hits).toBe(1);
-            expect(table.rows[7].cells[7].className).toBe("hit");
-            
-        })
-    })
-}) 
+			expect(table).not.toBeUndefined();
+		});
+	});
+});
