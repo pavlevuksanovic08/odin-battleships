faf928f151e6952984ae8d29c5dcf1e68bd8aa0a
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2025-12-19 01:13:04 +0100

    add attack function to RealPlayer what handles table  click event

--- src/player.js
+++ src/player.js
@@ -1,15 +1,42 @@
-import { Gameboard } from "./gameboard"
+import { Gameboard } from "./gameboard";
+import { Ship } from "./ship";
 
 export class Player {
-    constructor() {
-        this.gameboard = new Gameboard();
+  constructor() {
+    if (new.target === Player) {
+        throw new Error("Player is abstract and cannot be instantiated directly");
     }
+    this.gameboard = new Gameboard();
+  }
+  async attack() {
+    throw new Error("attack() must me implemented by subclass");
+  }
 }
 
 export class RealPlayer extends Player {
+  async attack(gameboard, table) {
+    const pos = await this.#waitForClick(table);
+    const x = Number(pos.x);
+    const y = Number(pos.y);
+    gameboard.receiveAttack(x, y);
+    if (gameboard.board[x][y] instanceof Ship)
+      table.rows[x].cells[y].className = "hit";
+    else table.rows[x].cells[y].className = "miss";
+  }
 
+   #waitForClick(table) {
+    return new Promise((resolve) => {
+      const handler = (event) => {
+        if (event.target.tagName !== "TD") return;
+        const x = event.target.dataset.x;
+        const y = event.target.dataset.y;
+        if (event.target.className) return; // ignore already-attacked cells
+        table.removeEventListener("click", handler);
+        resolve({ x: x, y: y });
+      };
+      table.addEventListener("click", handler);
+    });
+  }
 }
 
-export class ComputerPlayer extends Player {
-    
-}
+export class ComputerPlayer extends Player {}
--- src/player.test.js
+++ src/player.test.js
@@ -1,15 +1,79 @@
-import { RealPlayer, ComputerPlayer } from "./player";
+import { RealPlayer, ComputerPlayer, Player } from "./player";
+import { Gameboard } from "./gameboard";
+import DomUtils from "./domUtils";
+import { BoardRender } from "./boardRender";
+import { Game } from "./game";
+
+test("Player object cant be created", () => {
+	expect(() => new Player()).toThrow();
+});
 
 describe("RealPlayer class", () => {
-    test("check existance", () => {
+	test("check existance", () => {
+		const player = new RealPlayer();
+		expect(player).toBeDefined();
+	});
+
+	test("attack function", async () => {
+		const player = new RealPlayer();
+		const gameboard = new Gameboard();
+		const placeholder = DomUtils.createElement("div");
+
+		const table = BoardRender.load(gameboard, placeholder);
+
+		const attackPromise = player.attack(gameboard, table);
+
+		table.rows[3].cells[5].click();
+
+		await attackPromise;
+
+		expect(gameboard.board[3][5]).toBe("miss");
+		expect(table.rows[3].cells[5].className).toBe("miss");
+
+		gameboard.placeShip([7, 7], [7, 7]);
+
+		const attackPromise2 = player.attack(gameboard, table);
+
+		table.rows[7].cells[7].click();
+
+		await attackPromise2;
+
+		expect(gameboard.board[7][7].hits).toBe(1);
+		expect(table.rows[7].cells[7].className).toBe("hit");
+	});
+    test("same place can't be attacked twice", async () => {
         const player = new RealPlayer();
-        expect(player).toBeDefined();
-    })
-})
+        const gameboard = new Gameboard();
+        const placeholder = DomUtils.createElement("div");
+        const table = BoardRender.load(gameboard, placeholder);
+
+        // FIRST ATTACK
+        const attack1 = player.attack(gameboard, table);
+        table.rows[7].cells[7].click();
+        await attack1;
+
+        expect(table.rows[7].cells[7].className).toBe("miss");
+        expect(gameboard.board[7][7]).toBe("miss");
+
+        // SECOND ATTACK
+        const attack2 = player.attack(gameboard, table);
+
+        table.rows[7].cells[7].click(); // ignored
+        table.rows[8].cells[8].click(); // valid
+
+        await attack2;
+
+        expect(table.rows[7].cells[7].className).toBe("miss");
+        expect(gameboard.board[7][7]).toBe("miss");
+
+        expect(table.rows[8].cells[8].className).toBe("miss");
+        expect(gameboard.board[8][8]).toBe("miss");
+    });
+});
 
 describe("ComputerPlayer class", () => {
-    test("check existance", () => {
-        const player = new ComputerPlayer();
-        expect(player).toBeDefined();
-    })
-})
+	test("check existance", () => {
+		const player = new ComputerPlayer();
+		expect(player).toBeDefined();
+	});
+});
